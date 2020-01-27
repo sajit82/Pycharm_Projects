74bfadf7011a19a1d36458f1649d5ac45b3e4b4d
smca041 <smca041@gmail.com>
2020-01-28 00:07:45 +0530

    from pycharm

--- Test_Functions.py
+++ Test_Functions.py
@@ -1,7 +1,10 @@
-#Program to call a function
+# Program to call a function
 def test_function():
-    print("This is called from a function")
+    print("This is called from a functionsss")
+def greet():
+    return ("Hello")
+
 print("Hello")
 test_function()
 
-
+print(greet(),"Sajit")
