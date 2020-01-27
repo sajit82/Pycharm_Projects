068c67732fdf644745290544c8886e2d306dd4f2
smca041 <smca041@gmail.com>
2020-01-28 00:36:40 +0530

    from pycharm

--- Test_Functions.py
+++ Test_Functions.py
@@ -8,3 +8,24 @@ print("Hello")
 test_function()
 
 print(greet(),"Sajit")
+
+def stuff():
+    print('Hello')
+   # return
+    print('World')
+
+stuff()
+
+def computepay(h,r):
+    if h>40:
+       gp= (40*r) + ((h-40)*(1.5*r))
+    else:
+       gp = h*r
+    return gp
+
+hrs = input("Enter Hours:")
+rate= input("Enter Rate:")
+hr = float(hrs)
+ra =float(rate)
+p = computepay(hr,ra)
+print("Pay",p)
