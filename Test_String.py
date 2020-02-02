49d15bb014bc601eb5ccb932480388857dce2b27
smca041 <smca041@gmail.com>
2020-02-02 12:33:59 +0530

    from pycharm

--- Test_String.py
+++ Test_String.py
@@ -2,3 +2,19 @@ fruit = 'banana'
 
 for letter in fruit:
     print(letter)
+greet ='HELLO'
+zap =greet.lower()
+print(zap)
+pos =greet.find('EL')
+print(pos)
+nstr=greet.replace('L','X')
+print(nstr)
+whtspc =greet. lstrip()
+print(whtspc)
+gmail='smca041@gmail.com'
+atpos=gmail.find('@')
+print(atpos)
+sppos=gmail.find('.',atpos)
+print(sppos)
+data=gmail[atpos+1:sppos]
+print(data)
