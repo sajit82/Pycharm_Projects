66a43630333256e891652064bd1fb06601043212
smca041 <smca041@gmail.com>
2020-02-15 13:44:46 +0530

    from pycharm

---
+++ Test_LIST.py
@@ -0,0 +1,12 @@
+friends = ['sajit','ram','vivek']
+for name in friends:
+    print(name)
+print("Done")
+print(len(friends))
+print(range(len(friends)))
+print (range(4))
+
+for i in range(len(friends)):
+    print (friends[i])
+
+print(i)
