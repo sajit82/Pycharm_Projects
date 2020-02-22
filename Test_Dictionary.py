37831f0ad51b131a1428dbc29a89871526cbbe8b
smca041 <smca041@gmail.com>
2020-02-22 11:53:48 +0530

    from pycharm

--- Test_Dictionary.py
+++ Test_Dictionary.py
@@ -6,3 +6,14 @@ purse['candy']=3
 print(purse)
 print(purse['money'])
 print(purse['candy'])
+
+#getting the count of items in a dict using get() method
+
+count = dict()
+number =['343','234', '444','432','343']
+print(count)
+
+for num in number:
+    count[num]=count.get(num,0)+1
+print(count)
+
